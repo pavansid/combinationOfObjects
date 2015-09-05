3ee6e4c86544e2d79c1321b8c5e37feffb6c677c
Student <student@Achiever61.local>
2015-09-05 14:05:12 -0700

    Initial Commit

---
+++ combinationOfObjects/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  combinationOfObjects
+//
+//  Created by Student on 05/09/15.
+//  Copyright (c) 2015 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ combinationOfObjects/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  combinationOfObjects
+//
+//  Created by Student on 05/09/15.
+//  Copyright (c) 2015 BRN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
